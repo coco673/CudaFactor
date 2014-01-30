512afefbc4556db575088ab3c6a2bac7957156f0
Tony Coriolle <tony.coriolle@gmail.com>
2014-01-31 00:13:06 +0100

    ajout du header

---
+++ Cuda/Src/header/pgcd.h
@@ -0,0 +1,25 @@
+/*
+ * pgcd.h
+ *
+ *  Created on: 28 janv. 2014
+ *      Author: tony
+ */
+
+#ifndef PGDC_H_
+#define PGCD_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <float.h>
+#include <math.h>
+#include <string.h>
+
+#define N 2000
+
+
+int pgcd(long double a, long double b);
+char *convert(long double a);
+char *equalNBit(char *res,int size);
+
+
+#endif /* PGCD_H_ */
