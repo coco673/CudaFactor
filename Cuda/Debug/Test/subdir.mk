776fea8c02e673d64e1797abeb97d40e6b9edbd1
Tony Coriolle <tonycoriolle@free.fr>
2014-05-07 18:23:17 +0200

    ajout des fichiers nsight

--- Cuda/Debug/Test/subdir.mk
+++ Cuda/Debug/Test/subdir.mk
@@ -33,16 +33,16 @@ C_DEPS += \
 Test/%.o: ../Test/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: NVCC Compiler'
-	/usr/local/cuda-6.0/bin/nvcc -I"/home/tony/CudaFactor/Cuda/Src/header" -G -g -lineinfo -O0 -gencode arch=compute_20,code=sm_20  -odir "Test" -M -o "$(@:%.o=%.d)" "$<"
-	/usr/local/cuda-6.0/bin/nvcc -I"/home/tony/CudaFactor/Cuda/Src/header" -G -g -lineinfo -O0 --compile  -x c -o  "$@" "$<"
+	/usr/local/cuda-6.0/bin/nvcc -I"/home/groupedev/CudaFactor/Cuda/Src/header" -G -g -lineinfo -O0 -gencode arch=compute_20,code=sm_20  -odir "Test" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-6.0/bin/nvcc -I"/home/groupedev/CudaFactor/Cuda/Src/header" -G -g -lineinfo -O0 --compile  -x c -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 Test/%.o: ../Test/%.cu
 	@echo 'Building file: $<'
 	@echo 'Invoking: NVCC Compiler'
-	/usr/local/cuda-6.0/bin/nvcc -I"/home/tony/CudaFactor/Cuda/Src/header" -G -g -lineinfo -O0 -gencode arch=compute_20,code=sm_20  -odir "Test" -M -o "$(@:%.o=%.d)" "$<"
-	/usr/local/cuda-6.0/bin/nvcc --device-c -G -I"/home/tony/CudaFactor/Cuda/Src/header" -O0 -g -gencode arch=compute_20,code=compute_20 -gencode arch=compute_20,code=sm_20 -lineinfo  -x cu -o  "$@" "$<"
+	/usr/local/cuda-6.0/bin/nvcc -I"/home/groupedev/CudaFactor/Cuda/Src/header" -G -g -lineinfo -O0 -gencode arch=compute_20,code=sm_20  -odir "Test" -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-6.0/bin/nvcc --device-c -G -I"/home/groupedev/CudaFactor/Cuda/Src/header" -O0 -g -gencode arch=compute_20,code=compute_20 -gencode arch=compute_20,code=sm_20 -lineinfo  -x cu -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
