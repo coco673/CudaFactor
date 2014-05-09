609105076fd0b2ef421a42f8c367ddcc9f63721e
JaffarStiltskin <clement.etendard@gmail.com>
2014-05-09 18:40:58 +0200

    modif install.sh pour rectification ajout bashrc

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+grep libSage ~/.bashrc
+if [ $? != 0];then
+	echo "export PATH=$PWD/Sage/libSage:$PATH" >> ~/.bashrc 
+fi
 
-echo "export PATH= Sage/libSage:$PATH" >> ~/.bashrc 
 path=${PWD}
 
 if [ -e ${path}/Cuda/Debug/makefile ];then
